78621bd9fbd5741f83c227ef667cc27b529f6c9b
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-10-31 16:22:34 -0200

    just-playing with categories
    Added some code to turn text features to categoric integers and dummy
    columns

--- Module2/just-playing.py
+++ Module2/just-playing.py
@@ -10,11 +10,45 @@ Just playing with pandas
 import pandas as pd
 
 tutorial = pd.read_csv('./Datasets/tutorial.csv')
-print("{:=^30}".format(' HEAD '))
+print("{:=^40}".format(' HEAD '))
 print(tutorial.head(5))
-print("{:=^30}".format(' DESCRIPTION '))
+print("{:=^40}".format(' DESCRIPTION '))
 print(tutorial.describe())
-print("{:=^30}".format(' INDEX '))
+print("{:=^40}".format(' INDEX '))
 print(tutorial.index)
-print("{:=^30}".format(' DTYPES '))
+print("{:=^40}".format(' DTYPES '))
 print(tutorial.dtypes)
+
+print('='*40)
+print('{:=^40}'.format(' Changing text to categorical int '))
+print('='*40)
+ordered_satisfaction = ['Very Unhappy', 'Unhappy',
+                        'Neutral', 'Happy', 'Very Happy']
+df = pd.DataFrame({'satisfaction': ['Mad', 'Happy', 'Unhappy', 'Neutral']})
+print('\nSatisfaction as text')
+print(df)
+df.satisfaction = df.satisfaction.astype("category", ordered=True,
+                                         categories=ordered_satisfaction
+                                         ).cat.codes
+
+print('\nSatisfaction as int category')
+print(df)
+
+df2 = pd.DataFrame({'vertebrates': ['Bird',
+                                    'Bird',
+                                    'Mammal',
+                                    'Fish',
+                                    'Amphibian',
+                                    'Reptile',
+                                    'Mammal',
+                                    ]})
+
+print('\nVertebrates as text')
+print(df2)
+# Method 1)
+print('\nVertebrates as int')
+print(df2.vertebrates.astype("category").cat.codes)
+
+# Method 2)
+print('\nvertebrates as dummy columns')
+print(pd.get_dummies(df2, columns=['vertebrates']))
