2a8f6ee4a2641951c35d1ead061d2c89e52162be
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-10-31 16:36:42 -0200

    basic-text-processing
    Just the basic version of CountVectorizer()

---
+++ Module2/playing-with-text-and-images.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 16:18:45 2016
+
+@author: Arthur Gouveia
+
+Intro to text, image and sound processing
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+corpus = [
+          "Authman ran faster than Harry because he is an athlete.",
+          "Authman and Harry ran faster and faster."]
+
+bow = CountVectorizer()  # Bag Of Words
+X = bow.fit_transform(corpus)  # Sparse Matrix
+print(bow.get_feature_names())
+print('\n', X)
+print('\n', X.toarray())
