33e8a9325f86d9f2afa83a3f0bb56d361c2e991e
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-01 12:25:19 -0200

    Playing with sounds
    Read a WAV file for future machine learning techniques

--- Module2/playing-with-text-and-images.py
+++ Module2/playing-with-text-and-images.py
@@ -10,6 +10,8 @@ Intro to text, image and sound processing
 from sklearn.feature_extraction.text import CountVectorizer
 from scipy import misc
 import matplotlib.pyplot as plt
+import scipy.io.wavfile as wavefile
+import pandas as pd
 
 corpus = [
           "Authman ran faster than Harry because he is an athlete.",
@@ -26,15 +28,35 @@ img = misc.face() / 255
 print('Original image size: {}'.format(img.shape))
 img = img[::2, ::2]
 print('Reduced image size: {}'.format(img.shape))
+# Plots the original image
+plt.subplot(221)
+plt.imshow(img)
+plt.axis('off')
 
 # Convert the image to grayscale
 r, g, b = (img[..., i] for i in range(3))
 img = (0.299*r + 0.587*g + 0.114*b)
 print('Grayscale image size: {}'.format(img.shape))
-
+# Plots the gray image
+plt.subplot(222)
 # We need to set the colormap since this is just a lumnosity image
 # and plt.imshow uses jet as the default colormap
 # http://matplotlib.org/examples/color/colormaps_reference.html
 plt.imshow(img, cmap=plt.set_cmap('gray'))
+plt.axis('off')
+
 img = img.reshape(-1)
-# Now img is ready for machine learning
+# Now img is ready for machine learning or even some data exploration
+
+
+# Reading a WAV file
+sample_rate, audio_data = wavefile.read('Datasets/Alarm01.wav')
+print('\n\nAudio file sample rate', sample_rate, '\n')
+print(pd.DataFrame(audio_data).describe())
+
+plt.subplot(223)
+plt.plot(audio_data[:, 0])
+
+
+plt.subplot(224)
+plt.plot(audio_data[:, 1])
