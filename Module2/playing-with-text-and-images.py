10ca99d52e1d5e0e95b8019be377936325ce7443
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-01 02:33:06 -0200

    working with images

--- Module2/playing-with-text-and-images.py
+++ Module2/playing-with-text-and-images.py
@@ -8,13 +8,33 @@ Intro to text, image and sound processing
 """
 
 from sklearn.feature_extraction.text import CountVectorizer
+from scipy import misc
+import matplotlib.pyplot as plt
 
 corpus = [
           "Authman ran faster than Harry because he is an athlete.",
           "Authman and Harry ran faster and faster."]
 
-bow = CountVectorizer(ngram_range=(2,3))  # Bag Of Words
+bow = CountVectorizer(ngram_range=(2, 3), stop_words='english')  # Bag Of Words
 X = bow.fit_transform(corpus)  # Sparse Matrix
 print(bow.get_feature_names())
 print('\n', X)
 print('\n', X.toarray())
+
+# Opens 'face' image and normalizes RGB values from 0 to 1
+img = misc.face() / 255
+print('Original image size: {}'.format(img.shape))
+img = img[::2, ::2]
+print('Reduced image size: {}'.format(img.shape))
+
+# Convert the image to grayscale
+r, g, b = (img[..., i] for i in range(3))
+img = (0.299*r + 0.587*g + 0.114*b)
+print('Grayscale image size: {}'.format(img.shape))
+
+# We need to set the colormap since this is just a lumnosity image
+# and plt.imshow uses jet as the default colormap
+# http://matplotlib.org/examples/color/colormaps_reference.html
+plt.imshow(img, cmap=plt.set_cmap('gray'))
+img = img.reshape(-1)
+# Now img is ready for machine learning
