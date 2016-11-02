cbfae18990a1e09f194d877ba6d008ec91818a53
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-02 21:34:34 -0200

    Assignment 2 done
    In this assignment a CSV file is loaded, the features description is
    printed and also a slice of the dataframe

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -1,20 +1,15 @@
 import pandas as pd
 
 # TODO: Load up the 'tutorial.csv' dataset
-#
-# .. your code here ..
 
-
+tutorial = pd.read_csv('Datasets/tutorial.csv')
 
 # TODO: Print the results of the .describe() method
-#
-# .. your code here ..
 
-
+print(tutorial.describe())
 
 # TODO: Figure out which indexing method you need to
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
-#
-# .. your code here ..
 
+print(tutorial.ix[2:4, 'col3'])
