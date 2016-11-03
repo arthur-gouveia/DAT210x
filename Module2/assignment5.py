e9031b7f19c8b515474095b74f62fb584c884f89
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-03 17:02:26 -0200

    Assignment 5
    The greatest assignment so far. Read a CSV into a data frame, correct
    dtypes, check data quality,  change datatype of ordinal features and
    change nominal features to dummies

--- Module2/assignment5.py
+++ Module2/assignment5.py
@@ -1,14 +1,16 @@
 import pandas as pd
-import numpy as np
-
+#import numpy as np
 
 #
 # TODO:
 # Load up the dataset, setting correct header labels.
 #
-# .. your code here ..
-
-
+census = pd.read_csv('Datasets/census.data',
+                     index_col=0,
+                     names=['education', 'age', 'capital-gain', 'race',
+                            'capital-loss', 'hours-per-week', 'sex',
+                            'classification'],
+                     na_values='Preschool')
 
 #
 # TODO:
@@ -23,9 +25,29 @@ import numpy as np
 # should be represented as nans, you can convert them using
 # na_values when loading the dataframe.
 #
-# .. your code here ..
+print('Original dtypes')
+print(census.dtypes)
+census['capital-gain'] = pd.to_numeric(census['capital-gain'], errors='coerce')
+print('\n\nCorrected dtypes')
+print(census.dtypes)
 
+is_str_col = [type(census.loc[0, col]) == str for col in census.columns]
+str_cols = census.columns[is_str_col]
+num_cols = census.columns[[not(x) for x in is_str_col]]
 
+# Numeric summary of numerical columns
+print(census[num_cols].describe())
+# Frequency table for nominal columns
+for col in str_cols:
+    print('\nCount of unique values of "{}":\n{}'.format(col,
+                                                         census[col].
+                                                         value_counts()))
+
+# Things that called my attentions:
+#   1 - 51 people declared 'Preschool' as education level
+#   2 - min and max hours per week: 1 and 99
+#   3 - max capital-gain: 99999
+#   4 - ages == 90
 
 #
 # TODO:
@@ -38,14 +60,35 @@ import numpy as np
 # make more sense to have a numeric type or a series of categories
 # for these somewhat ambigious features?
 #
-# .. your code here ..
+# Ordinal categories
 
+params = ('ordered=True', 'categories=set(census.classification)')
+census.classification = census.classification.astype("category",
+                                                     *params).cat.codes
+education_categories = ['1st-4th',
+                        '5th-6th',
+                        '7th-8th',
+                        '9th',
+                        '10th',
+                        '11th',
+                        '12th',
+                        'HS-grad',
+                        'Some-college',
+                        'Bachelors',
+                        'Masters',
+                        'Doctorate']
 
+census.education = census.education.astype('category', ordered=True,
+                                           categories=education_categories
+                                           ).cat.codes
+
+# Nominal categories
+
+census = pd.get_dummies(census, columns=['sex', 'race'])
 
 #
 # TODO:
 # Print out your dataframe
 #
 # .. your code here ..
-
-
+print(census)
