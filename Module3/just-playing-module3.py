56cd58357e066d386012c2856521a8058c29ec9c
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-06 14:52:17 -0200

    corrected kwargs and function names
    Function names can't start with numbers and the correct form is **kwargs
    not *kwargs

--- Module3/just-playing-module3.py
+++ Module3/just-playing-module3.py
@@ -11,15 +11,15 @@ http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.hist
 """
 
 
-def histplot(data, *kwargs):
-    data.plot.hist(*kwargs)
+def histplot(data, **kwargs):
+    data.plot.hist(**kwargs)
 
 
-def 2Dscatter(data, x, y, *kwargs):
-    data.plot.scatter(x=x, y=y, *kwargs)
+def scatter2D(data, x, y, **kwargs):
+    data.plot.scatter(x=x, y=y, **kwargs)
 
 
-def 3Dscatter(data, *kwargs):
+def scatter3D(data, **kwargs):
     fig = mpl.pyplot.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel('Final Grade')
@@ -27,7 +27,7 @@ def 3Dscatter(data, *kwargs):
     ax.set_zlabel('Daily Alcohol')
 
     ax.scatter(data[0], data[1],
-               data[2], *kwargs)
+               data[2], **kwargs)
     mpl.pyplot.show()
 
 
@@ -40,17 +40,10 @@ mpl.cm.cmapname = 'gray'
 
 student_dataset = pd.read_csv("Datasets/students.data", index_col=0)
 
-histpot(student_dataset.G3, alpha=0.5, normed=True)
-histpot(student_dataset[['G3', 'G2', 'G1']], alpha=0.5)
+histplot(student_dataset.G3, alpha=0.5, normed=True)
+histplot(student_dataset[['G3', 'G2', 'G1']], alpha=0.5)
 
-student_dataset[['G1', 'G3']].plot.scatter(x='G1', y='G3')
+scatter2D(student_dataset[['G1', 'G3']], x='G1', y='G3')
 
-fig = mpl.pyplot.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('Final Grade')
-ax.set_ylabel('First Grade')
-ax.set_zlabel('Daily Alcohol')
-
-ax.scatter(student_dataset.G1, student_dataset.G3,
-           student_dataset['Dalc'], c='r', marker='o')
-mpl.pyplot.show()
+scatter3D([student_dataset.G1, student_dataset.G3,
+           student_dataset['Dalc']], c='r', marker='o')
