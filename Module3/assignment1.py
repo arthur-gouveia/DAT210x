7a581d3a7c2c1c588b9cee0661977a3c4b558321
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-07 00:13:42 -0200

    Assignment 1
    Histograms of wheat.data

--- Module3/assignment1.py
+++ Module3/assignment1.py
@@ -3,36 +3,34 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 # Look pretty...
-matplotlib.style.use('ggplot')
+matplotlib.style.use('seaborn-deep')
 
 #
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
-# 
-# .. your code here ..
-
+#
+df = pd.read_csv('Datasets/wheat.data', index_col=0)
 
 #
 # TODO: Create a slice of your dataframe (call it s1)
 # that only includes the 'area' and 'perimeter' features
-# 
-# .. your code here ..
+#
+s1 = df[['area', 'perimeter']]
 
 
 #
 # TODO: Create another slice of your dataframe (call it s2)
 # that only includes the 'groove' and 'asymmetry' features
-# 
-# .. your code here ..
+#
+s2 = df[['groove', 'asymmetry']]
 
 
 #
 # TODO: Create a histogram plot using the first slice,
 # and another histogram plot using the second slice.
 # Be sure to set alpha=0.75
-# 
-# .. your code here ..
-
+#
+s1.plot.hist(s1, alpha=0.75)
+s2.plot.hist(s2, alpha=0.75)
 
 plt.show()
-
