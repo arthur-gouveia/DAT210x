f2314af7be4a23359c73480f42213f179960c988
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-07 01:21:47 -0200

    Assignment 5
    Andrews Curves

--- Module3/assignment5.py
+++ Module3/assignment5.py
@@ -1,4 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+from pandas.tools.plotting import andrews_curves
+
+# Look pretty...
+matplotlib.style.use('ggplot')
+drp = input('Drop features? [y/n]: ')
+
 #
-# This code is intentionally missing!
-# Read the directions on the course lab page!
+# TODO: Load up the Seeds Dataset into a Dataframe
+# It's located at 'Datasets/wheat.data'
 #
+df = pd.read_csv('Datasets/wheat.data', index_col=0)
+
+
+
+#
+# TODO: Drop the 'id', 'area', and 'perimeter' feature
+#
+if drp == 'y':
+    df.drop(['area', 'perimeter'], axis=1, inplace=True)
+
+
+
+#
+# TODO: Plot an Andrews Curve chart grouped by
+# the 'wheat_type' feature. Be sure to set the optional
+# display parameter alpha to 0.4
+#
+
+plt.figure()
+
+andrews_curves(df, 'wheat_type', alpha=0.4)
+
+plt.show()
+
