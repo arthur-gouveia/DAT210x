b550268af36e42079ed79a16c3bc6c296fe708ca
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-07 00:52:51 -0200

    Assignment 3
    3D scatterplot

--- Module3/assignment3.py
+++ Module3/assignment3.py
@@ -10,9 +10,8 @@ matplotlib.style.use('ggplot')
 #
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
-# 
-# .. your code here ..
-
+#
+df = pd.read_csv('Datasets/wheat.data', index_col=0)
 
 
 fig = plt.figure()
@@ -22,21 +21,28 @@ fig = plt.figure()
 # perimeter and asymmetry features. Be sure to use the
 # optional display parameter c='red', and also label your
 # axes
-# 
-# .. your code here ..
+#
+
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('area')
+ax.set_ylabel('perimeter')
+ax.set_zlabel('asymmetry')
+ax.scatter(df.area, df.perimeter, df.asymmetry, c='red')
 
 
-fig = plt.figure()
 #
 # TODO: Create a new 3D subplot using fig. Then use the
 # subplot to graph a 3D scatter plot using the width,
 # groove and length features. Be sure to use the
 # optional display parameter c='green', and also label your
 # axes
-# 
-# .. your code here ..
+#
+fig2 = plt.figure()
+ax = fig2.add_subplot(111, projection='3d')
+ax.set_xlabel('width')
+ax.set_ylabel('groove')
+ax.set_zlabel('length')
+ax.scatter(df.width, df.groove, df.length, c='red')
 
 
 plt.show()
-
-
