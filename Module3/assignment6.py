1cd56d2a3b4873764f19a9c884f386a770dd4a87
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-07 01:38:19 -0200

    Assignment 6
    Plotting the correlation matrix

--- Module3/assignment6.py
+++ Module3/assignment6.py
@@ -1,32 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 
-
+matplotlib.style.use('seaborn-dark')
 #
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
-# 
-# .. your code here ..
+#
+df = pd.read_csv('Datasets/wheat.data', index_col=0)
 
 
 #
 # TODO: Drop the 'id' feature
-# 
-# .. your code here ..
+#
+# already dropped when reading the csv
 
 
 #
 # TODO: Compute the correlation matrix of your dataframe
-# 
-# .. your code here ..
+#
+cormatrix = df.corr()
 
 
 #
 # TODO: Graph the correlation matrix using imshow or matshow
-# 
-# .. your code here ..
+#
+plt.imshow(cormatrix, cmap=plt.cm.Blues, interpolation='nearest')
+plt.colorbar()
+tick_marks = [i for i in range(len(df.columns[:7]))]
+plt.xticks(tick_marks, df.columns[:7], rotation='vertical')
+plt.yticks(tick_marks, df.columns[:7])
 
+plt.matshow(cormatrix, cmap=plt.cm.Reds)
+plt.colorbar()
+tick_marks = [i for i in range(len(df.columns[:7]))]
+plt.xticks(tick_marks, df.columns[:7], rotation='vertical')
+plt.yticks(tick_marks, df.columns[:7])
 
 plt.show()
-
-
