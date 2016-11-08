bd87ca5df340aaa48eca521cedf9bfd628e6c9a9
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-08 16:30:57 -0200

    Assignment2 completed
    I also made a little change in the script so that if you call it with -s
    as a parameter the script will scale the features. Otherwise it will
    leave the features as they are

--- Module4/assignment2.py
+++ Module4/assignment2.py
@@ -2,34 +2,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import assignment2_helper as helper
+from sklearn.decomposition import PCA
+import sys
 
 # Look pretty...
 matplotlib.style.use('ggplot')
 
 
-# Do * NOT * alter this line, until instructed!
-scaleFeatures = False
-
+# You can set scaleFeatures to True by passing -s as a parameter to the script
+if (len(sys.argv) == 2) and (sys.argv[1] == '-s'):
+    scaleFeatures = True
+else:
+    scaleFeatures = False
 
 # TODO: Load up the dataset and remove any and all
 # Rows that have a nan. You should be a pro at this
 # by now ;-)
 #
-# .. your code here ..
-
+df = pd.read_csv('Datasets/kidney_disease.csv', index_col=0)
+df.dropna(inplace=True)
 
 
 # Create some color coded labels; the actual label feature
 # will be removed prior to executing PCA, since it's unsupervised.
 # You're only labeling by color so you can see the effects of PCA
-labels = ['red' if i=='ckd' else 'green' for i in df.classification]
+labels = ['red' if i == 'ckd' else 'green' for i in df.classification]
 
 
 # TODO: Use an indexer to select only the following columns:
 #       ['bgr','wc','rc']
 #
-# .. your code here ..
-
+df = df[['bgr', 'wc', 'rc']]
 
 
 # TODO: Print out and check your dataframe's dtypes. You'll probably
@@ -43,8 +46,8 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # properly detect and convert them to that data type for you, then use
 # an appropriate command to coerce these features into the right type.
 #
-# .. your code here ..
-
+df['wc'] = pd.to_numeric(df['wc'], errors='coerce')
+df['rc'] = pd.to_numeric(df['rc'], errors='coerce')
 
 
 # TODO: PCA Operates based on variance. The variable with the greatest
@@ -56,24 +59,24 @@ labels = ['red' if i=='ckd' else 'green' for i in df.classification]
 # Hint: If you don't see all three variables: 'bgr','wc' and 'rc', then
 # you probably didn't complete the previous step properly.
 #
-# .. your code here ..
-
+print('Old variance:\n', df.var())
+print('Old describe:\n', df.describe())
 
 
 # TODO: This method assumes your dataframe is called df. If it isn't,
 # make the appropriate changes. Don't alter the code in scaleFeatures()
 # just yet though!
 #
-# .. your code adjustment here ..
-if scaleFeatures: df = helper.scaleFeatures(df)
-
+if scaleFeatures:
+    df = helper.scaleFeatures(df)
 
 
 # TODO: Run PCA on your dataset and reduce it to 2 components
 # Ensure your PCA instance is saved in a variable called 'pca',
 # and that the results of your transformation are saved in 'T'.
 #
-# .. your code here ..
+pca = PCA(2)
+T = pca.fit_transform(df)
 
 
 # Plot the transformed data as a scatter plot. Recall that transforming
@@ -86,10 +89,12 @@ if scaleFeatures: df = helper.scaleFeatures(df)
 #
 # Since we transformed via PCA, we no longer have column names. We know we
 # are in P.C. space, so we'll just define the coordinates accordingly:
-ax = helper.drawVectors(T, pca.components_, df.columns.values, plt, scaleFeatures)
+plt.figure()
+ax = helper.drawVectors(T, pca.components_, df.columns.values,
+                        plt, scaleFeatures)
 T = pd.DataFrame(T)
 T.columns = ['component1', 'component2']
-T.plot.scatter(x='component1', y='component2', marker='o', c=labels, alpha=0.75, ax=ax)
+T.plot.scatter(x='component1', y='component2', marker='o',
+               c=labels, alpha=0.75, ax=ax)
+plt.title('Scaled' if scaleFeatures else 'Unscaled')
 plt.show()
-
-
