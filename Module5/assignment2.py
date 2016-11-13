d9c546b59a4a92ad5e06fe9b80b84f8c15853c0a
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-11-13 21:54:47 -0200

    Loop through the phones list
    Now the whole assignment is inside a for loop

--- Module5/assignment2.py
+++ Module5/assignment2.py
@@ -26,7 +26,6 @@ cdr = pd.read_csv('Datasets/CDR.csv')
 cdr.CallDate = pd.to_datetime(cdr.CallDate)
 cdr.CallTime = pd.to_timedelta(cdr.CallTime)
 
-
 #
 # TODO: Get a distinct list of "In" phone numbers (users) and store the values
 # in a regular python list.
@@ -35,57 +34,58 @@ cdr.CallTime = pd.to_timedelta(cdr.CallTime)
 #
 unique_in = cdr.In.unique().tolist()
 
+#
+# TODO: Repeat the above steps for all 10 individuals, being sure to record
+# their approximate home locations. You might want to use a for-loop, unless
+# you enjoy typing.
+#
+for phone in unique_in:
+    #
+    # TODO: Create a slice called user1 that filters to only include dataset
+    # records where the "In" feature (user phone number) is equal to the
+    # current number on your unique list above
+    #
+    user1 = cdr[cdr.In == phone]
+    # INFO: Plot all the call locations
+    user1.plot.scatter(x='TowerLon', y='TowerLat', c='gray', alpha=0.1,
+                       title='Call Locations')
+#    showandtell('Call Locations ', phone)
 
 #
-# TODO: Create a slice called user1 that filters to only include dataset
-# records where the "In" feature (user phone number) is equal to the first
-# number on your unique list above
-#
-user1 = cdr[cdr.In == unique_in[0]]
-
-# INFO: Plot all the call locations
-user1.plot.scatter(x='TowerLon', y='TowerLat', c='gray', alpha=0.1,
-                   title='Call Locations')
-showandtell()  # Comment this line out when you're ready to proceed
-
-
-#
-# INFO: The locations map above should be too "busy" to really wrap your head
-# around. This is where domain expertise comes into play. Your intuition tells
-# you that people are likely to behave differently on weekends:
+# INFO: The locations map above should be too "busy" to really wrap your
+# head around. This is where domain expertise comes into play. Your
+# intuition tells you that people are likely to behave differently on weekends:
 #
 # On Weekends:
 #   1. People probably don't go into work
 #   2. They probably sleep in late on Saturday
-#   3. They probably run a bunch of random errands, since they couldn't during
-#      the week
-#   4. They should be home, at least during the very late hours, e.g. 1-4 AM
+#   3. They probably run a bunch of random errands, since they couldn't
+#      during the week
+#   4. They should be home, at least during the very late hours, e.g. 1-4AM
 #
 # On Weekdays:
 #   1. People probably are at work during normal working hours
-#   2. They probably are at home in the early morning and during the late night
+#   2. They probably are at home in the early morning and during late night
 #   3. They probably spend time commuting between work and home everyday
-
-
 #
-# TODO: Add more filters to the user1 slice you created. Add bitwise logic so
-# that you're only examining records that came in on weekends (sat/sun).
+# TODO: Add more filters to the user1 slice you created. Add bitwise logic
+# so that you're only examining records that came in on weekends (sat/sun).
 #
-user1 = user1[(user1.DOW == 'Sat') | (user1.DOW == 'Sun')]
-
-
+    user1 = user1[(user1.DOW == 'Sat') | (user1.DOW == 'Sun')]
 #
-# TODO: Further filter it down for calls that are came in either before 6AM OR
-# after 10pm (22:00:00).
-# You can use < and > to compare the string times, just make sure you code them
-# as military time strings, eg: "06:00:00", "22:00:00":
+# TODO: Further filter it down for calls that are came in either before 6AM
+# OR after 10pm (22:00:00).
+# You can use < and > to compare the string times, just make sure you code
+# them as military time strings, eg: "06:00:00", "22:00:00":
 # https://en.wikipedia.org/wiki/24-hour_clock
 #
-# You might also want to review the Data Manipulation section for this. Once
-# you have your filtered slice, print out its length:
+# You might also want to review the Data Manipulation section for this.
+# Once you have your filtered slice, print out its length:
 #
-user1 = user1[(user1.CallTime > '22:00:00') | (user1.CallTime < '06:00:00')]
-print(len(user1))
+    user1 = user1[(user1.CallTime > '22:00:00') |
+                  (user1.CallTime < '06:00:00')]
+    print('This user has {} calls on weekends between 10PM and 6AM.'.format(
+          len(user1)))
 
 #
 # INFO: Visualize the dataframe with a scatter plot as a sanity check. Since
@@ -98,12 +98,11 @@ print(len(user1))
 # based off the cell phone tower position data; but considering the below are
 # for Calls that arrived in the twilight hours of weekends, it's likely that
 # wherever they are bunched up is probably near where the caller's residence:
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(user1.TowerLon, user1.TowerLat, c='g', marker='o', alpha=0.2)
-ax.set_title('Weekend Calls (<6am or >10p)')
-showandtell()  # TODO: Comment this line out when you're ready to proceed
-
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(user1.TowerLon, user1.TowerLat, c='g', marker='o', alpha=0.2)
+    ax.set_title('Weekend Calls (<6am or >10p)')
+#    showandtell('Weekend Calls (<6am or >10p)', phone)
 
 #
 # TODO: Run K-Means with a K=1. There really should only be a single area of
@@ -122,36 +121,6 @@ showandtell()  # TODO: Comment this line out when you're ready to proceed
 # Hint: Make sure you graph the CORRECT coordinates. This is part of your
 # domain expertise.
 #
-kmeans_model = KMeans(2, tol=1e-12)
-kmeans_model.fit(cdr[['TowerLon', 'TowerLat']])
-centroids = kmeans_model.cluster_centers_
-print(centroids)
-ax.scatter(centroids[:, 0], centroids[:, 1], marker='x', c='r', alpha=0.5,
-           linewidths=3, s=169)
-showandtell()  # TODO: Comment this line out when you're ready to proceed
-
-
-#
-# TODO: Repeat the above steps for all 10 individuals, being sure to record
-# their approximate home locations. You might want to use a for-loop, unless
-# you enjoy typing.
-#
-for phone in unique_in[1:]:
-    user1 = cdr[cdr.In == phone]
-    # INFO: Plot all the call locations
-    user1.plot.scatter(x='TowerLon', y='TowerLat', c='gray', alpha=0.1,
-                       title='Call Locations')
-#    showandtell('Call Locations ', phone)
-    user1 = user1[(user1.DOW == 'Sat') | (user1.DOW == 'Sun')]
-    user1 = user1[(user1.CallTime > '22:00:00') |
-                  (user1.CallTime < '06:00:00')]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(user1.TowerLon, user1.TowerLat, c='g', marker='o', alpha=0.2)
-    ax.set_title('Weekend Calls (<6am or >10p)')
-#    showandtell('Weekend Calls (<6am or >10p)', phone)
-
     kmeans_model = KMeans(2, tol=1e-12)
     kmeans_model.fit(cdr[['TowerLon', 'TowerLat']])
     centroids = kmeans_model.cluster_centers_
