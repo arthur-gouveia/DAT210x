04bd285ac23c4bae581afe75fc3a528e13aafc40
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2016-12-06 22:45:20 -0200

    Assignment 8 - Linear Regression
    Linear regression on life expectancy

--- Module5/assignment8.py
+++ Module5/assignment8.py
@@ -1,30 +1,31 @@
 import pandas as pd
-import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
 
-matplotlib.style.use('ggplot') # Look Pretty
+matplotlib.style.use('ggplot')  # Look Pretty
 
 
 def drawLine(model, X_test, y_test, title):
-  # This convenience method will take care of plotting your
-  # test observations, comparing them to the regression line,
-  # and displaying the R2 coefficient
-  fig = plt.figure()
-  ax = fig.add_subplot(111)
-  ax.scatter(X_test, y_test, c='g', marker='o')
-  ax.plot(X_test, model.predict(X_test), color='orange', linewidth=1, alpha=0.7)
+    # This convenience method will take care of plotting your
+    # test observations, comparing them to the regression line,
+    # and displaying the R2 coefficient
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(X_test, y_test, c='g', marker='o')
+    ax.plot(X_test, model.predict(X_test), color='orange',
+            linewidth=1, alpha=0.7)
+    TEXT_TO_PRINT = 'Est {} {} Life Expectancy: {}'
 
-  print "Est 2014 " + title + " Life Expectancy: ", model.predict([[2014]])[0]
-  print "Est 2030 " + title + " Life Expectancy: ", model.predict([[2030]])[0]
-  print "Est 2045 " + title + " Life Expectancy: ", model.predict([[2045]])[0]
+    for year in [2014, 2030, 2045]:
+        print(TEXT_TO_PRINT.format(year, title, model.predict([[year]])[0]))
 
-  score = model.score(X_test, y_test)
-  title += " R2: " + str(score)
-  ax.set_title(title)
+    score = model.score(X_test, y_test)
+    title += " R2: " + str(score)
+    ax.set_title(title)
 
-
-  plt.show()
+    plt.show()
 
 
 #
@@ -34,7 +35,7 @@ def drawLine(model, X_test, y_test, title):
 # spread sheet application
 #
 # .. your code here ..
-
+X = pd.read_csv('Datasets/life_expectancy.csv', sep='\t')
 
 #
 # TODO: Create your linear regression model here and store it in a
@@ -43,7 +44,7 @@ def drawLine(model, X_test, y_test, title):
 #
 # .. your code here ..
 
-
+model = LinearRegression()
 
 #
 # TODO: Slice out your data manually (e.g. don't use train_test_split,
@@ -55,7 +56,8 @@ def drawLine(model, X_test, y_test, title):
 #
 # .. your code here ..
 
-
+X_train = X.loc[X.Year < 1986, ['Year']]
+y_train = X.loc[X.Year < 1986, ['WhiteMale']]
 
 #
 # TODO: Train your model then pass it into drawLine with your training
@@ -66,25 +68,31 @@ def drawLine(model, X_test, y_test, title):
 # 2030 and 2045 extrapolation.
 #
 # .. your code here ..
-
+model.fit(X_train, y_train)
+drawLine(model, X_train, y_train, 'White Male Regression')
 
 #
 # TODO: Print the actual 2014 WhiteMale life expectancy from your
 # loaded dataset
 #
 # .. your code here ..
+plt.scatter(X.loc[X.Year == 2014, ['Year']],
+            X.loc[X.Year == 2014, ['WhiteMale']],
+            c='r')
 
-
-
-# 
+#
 # TODO: Repeat the process, but instead of for WhiteMale, this time
 # select BlackFemale. Create a slice for BlackFemales, fit your
 # model, and then call drawLine. Lastly, print out the actual 2014
 # BlackFemale life expectancy
 #
 # .. your code here ..
-
-
+y_train = X.loc[X.Year < 1986, ['BlackFemale']]
+model.fit(X_train, y_train)
+drawLine(model, X_train, y_train, 'Black Female Regression')
+plt.scatter(X.loc[X.Year == 2014, ['Year']],
+            X.loc[X.Year == 2014, ['BlackFemale']],
+            c='r')
 
 #
 # TODO: Lastly, print out a correlation matrix for your entire
@@ -94,10 +102,10 @@ def drawLine(model, X_test, y_test, title):
 #
 # .. your code here ..
 
+plt.figure()
+cormatrix = X.corr()
+sns.heatmap(cormatrix, annot=True, cmap=plt.cm.Blues, fmt='.3f')
 plt.show()
-
-
-
 
 #
 # INFO + HINT On Fitting, Scoring, and Predicting:
@@ -131,4 +139,3 @@ plt.show()
 # SKLearn expects your data to be arranged as [n_samples, n_features].
 # Keep in mind, all of the above only relates to your "X" or input
 # data, and does not apply to your "y" or labels.
-
